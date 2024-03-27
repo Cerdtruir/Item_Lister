636b51e4de62b96b043bf7fbb17541240a259044
Cerdtruir <ghoornabeel@gmail.com>
2024-03-27 14:49:08 +0200

    Improve csv output and set name

--- app/jobs/woocommerce_job.rb
+++ app/jobs/woocommerce_job.rb
@@ -2,14 +2,11 @@ class WoocommerceJob < ApplicationJob
   queue_as :default
 
   def perform
-    require 'csv'
-
-    # Fetch all items from the database
     items = Item.all
     headers = ['ID', 'Type', 'Name', 'Published', 'Is featured?', 'Visibility in catalog', 'Short description',
                'Description', 'Tax status', 'In stock?', 'Stock', 'Backorders allowed?', 'Sold individually?',
                'Allow customer reviews?', 'Regular price', 'Categories', 'Images']
-    CSV.open('public/items.csv', 'w', write_headers: true, headers:) do |writer|
+    CSV.open('public/woocommerce_items.csv', 'w', write_headers: true, headers:) do |writer|
       items.each do |item|
         in_stock = if item.quantity.zero?
                      0
@@ -17,8 +14,8 @@ class WoocommerceJob < ApplicationJob
                      1
                    end
 
-        writer << [item.id, 'simple', item.name, 1, 0, 'visible', item.description, item.description,
-                   'taxable', in_stock, item.quantity, 0, 0, 1, item.selling_price, 'items', "https://res.cloudinary.com/dwi7jdore/image/upload/#{item.id}.jpg"]
+        writer << [item.id, 'simple', "#{item.name} - #{item.condition}", 1, 0, 'visible', "#{item.description} - #{item.condition}", "#{item.description} - #{item.condition}",
+                   'taxable', in_stock, item.quantity, 0, 0, 1, item.selling_price, 'Products', "https://res.cloudinary.com/dwi7jdore/image/upload/#{item.id}.jpg"]
       end
     end
   end
