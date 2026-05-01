0486cdf3095430723278c7e8203cd6a6331ceeee
nabeel <ghoornabeel@gmail.com>
2026-05-01 16:30:41 +0200

    Update html ui

--- app/views/items/_item.json.jbuilder
+++ app/views/items/_item.json.jbuilder
@@ -1,2 +1,3 @@
-json.extract! item, :id, :name, :description, :condition, :quantity, :cost_price, :selling_price, :image, :created_at, :updated_at
+json.extract! item, :id, :name, :description, :condition, :quantity, :cost_price, :selling_price, :image, :created_at,
+              :updated_at, :external_stock
 json.url item_url(item, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  resources :items
+  get 'items/new_takealot', to: 'items#new_takealot', as: 'new_takealot_form'
+
+  resources :items do
+    collection do
+      post :create_from_takealot
+      get 'scan_barcode'
+      post 'create_from_barcode'
+      get 'mobile_scan'
+      post 'lookup_barcode'
+      post 'create_from_mobile_scan'
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'items#mobile_scan'
 end
+
